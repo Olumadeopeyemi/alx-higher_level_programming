51aa9bb532baac95002ea57785f72a6b34182f4a
root <dimejiolus@gmail>
2023-04-25 22:49:51 +0000

    commit

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
-// Gets the contents of a webpage and stores it in a file
-
-const request = require('request');
 const fs = require('fs');
+const request = require('request');
 const url = process.argv[2];
-const file = process.argv[3];
+const path = process.argv[3];
 
-request(url, function (err, res, body) {
+request(url, function (err, data, body) {
   if (err) {
     console.log(err);
+  } else {
+    fs.writeFile(path, body, 'utf8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
-  fs.writeFile(file, body, 'utf8', function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
 });
